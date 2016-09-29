ff6253df831893a2efb3239e19e8a506f84709bc
Nelson Chen <nechen@ap.corp.jwt.com>
2016-09-29 14:04:19 +0800

    update catalog files

--- app/js/app.js
+++ app/js/app.js
@@ -21,7 +21,9 @@ if(/localhost[:]9000/.test(location.href)){
 	$('.logo a').attr('href','/kymco-motors-productsite/');
 	$(function(){
 		$('.kv figure').each(function(i,d){
-			$(this).attr('data-src', $(this).attr('data-src').replace(/img\//ig,'/kymco-motors-productsite/img/'));
+			if($(this).attr('data-src')){
+				$(this).attr('data-src', $(this).attr('data-src').replace(/img\//ig,'/kymco-motors-productsite/img/'));
+			}
 		});
 	});
 }
